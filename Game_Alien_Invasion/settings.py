11ee32b2f369cd9c25518107b7f553d77337fb2c
mykola-444 <a-mykola@ukr.net>
2021-09-15 23:33:40 +0300

    continue work on project Alien_Invasion

--- Game_Alien_Invasion/settings.py
+++ Game_Alien_Invasion/settings.py
@@ -1,6 +1,7 @@
 class Settings:
     def __init__(self):
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 1  # ship speed - move to 1.5 picsel
--- Game_Alien_Invasion/ship.py
+++ Game_Alien_Invasion/ship.py
@@ -1,18 +1,29 @@
 import pygame
 
+
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('images/ship.bmp')  # load image of ship
         self.rect = self.image.get_rect()
+        self.x = float(self.rect.x)                       # float value for ship with speed 1.5)
 
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False  # motion indicator
+        self.moving_left = False
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
 
+    def update(self):
+        """ update new position of ship"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed     # update  ship.x instead rect
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
-
-
+        self.rect.x = self.x                        # update rect with self.x
